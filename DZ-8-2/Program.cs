e9cb5c4e52f0bfdf17767b5555e9605250fcf4fe
Dimitrey1978 <Dimitrey@mail.ru>
2021-03-12 10:31:31 +0300

    Добавьте файлы проекта.

---
+++ DZ-8-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DZ_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Количество клиентов (до 20): ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            long numberPaths = Faktorial(quantity);
+            Console.WriteLine($"Количество вариантов маршрутов: {numberPaths}");
+
+        }
+
+        static long Faktorial (int A)
+        {
+            if (A != 0)
+                {
+                return A*Faktorial(A-1);
+            }
+            else
+                return 1;
+        }
+    }
+}
